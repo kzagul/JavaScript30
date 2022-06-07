d684f60e964147a16ce50bf98f0e15137ee6953a
Kirill Zagul <zagulkirill@gmail.com>
2022-06-07 14:29:17 +0500

    1-st task done!

---
+++ 01-js-drumkit/js.js
@@ -0,0 +1,14 @@
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) =>
+  key.addEventListener("transitionend", (e) => {
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove("playing");
+  })
+);
+window.addEventListener("keydown", (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+});
